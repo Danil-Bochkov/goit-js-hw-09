48c0327b76918d27c1f47b9c171cd382153b41e4
Danil-Bochkov <106696666+Danil-Bochkov@users.noreply.github.com>
2022-08-31 21:13:03 +0300

    fix2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -3,11 +3,12 @@ import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 let time = 0;
+
+player.setCurrentTime(localStorage.getItem("videoplayer-current-time") || 0);
+
 const onPlay = function (data) { 
     time = localStorage.setItem("videoplayer-current-time", data.seconds);
     console.log(seconds);
-    // data is an object containing properties specific to that event
 };
 
 player.on('timeupdate', onPlay);
-// console.log(time);
